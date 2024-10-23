c4523d957558fde5a435f1e181a4fbcb75fc3551
Ivan Ferreira <ivan.marcelo.ferreira@gmail.com>
2024-10-23 20:28:42 -0300

    chore: adding findById

--- src/main/java/br/grupointegrado/movies/controller/MovieController.java
+++ src/main/java/br/grupointegrado/movies/controller/MovieController.java
@@ -4,6 +4,7 @@ import br.grupointegrado.movies.model.Movie;
 import br.grupointegrado.movies.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,13 @@ public class MovieController {
     @GetMapping
     public List<Movie> findAll() {
         return this.repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Movie findById(@PathVariable Integer id) {
+        return this.repository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Filme não foi encontrado"));
     }
 
 }
