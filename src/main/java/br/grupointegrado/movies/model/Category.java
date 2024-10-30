21b5a6bc979b5b82ecb8a87753c63ed569b701d2
Ivan Ferreira <ivan.marcelo.ferreira@gmail.com>
2024-10-30 20:22:14 -0300

    chore: add category model; add swagger

---
+++ src/main/java/br/grupointegrado/movies/model/Category.java
@@ -0,0 +1,31 @@
+package br.grupointegrado.movies.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
